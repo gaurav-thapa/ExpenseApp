356dcc36ac238a059d337651557a02bddd22c95c
Gaurav <thapagaurav7011@gmail.com>
2024-08-16 14:04:11 +0530

    final

--- src/App.js
+++ src/App.js
@@ -2,32 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-const DUMMY_EXPENSES = [
-    {
-        id: 1,
-        title: "Toilet Paper",
-        amount: 94.12,
-        date: new Date(2023, 7, 14),
-    },
-    {
-        id: 2,
-        title: "Rent",
-        amount: 1200,
-        date: new Date(2021, 12, 22),
-    },
-    {
-        id: 3,
-        title: "Groceries",
-        amount: 49.99,
-        date: new Date(2022, 5, 16),
-    },
-    {
-        id: 4,
-        title: "Skin Care",
-        amount: 99.12,
-        date: new Date(2023, 1, 1),
-    },
-];
+import { DUMMY_EXPENSES } from "./DUMMY_EXPENSE";
+
 function App() {
     const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
---
+++ src/DUMMY_EXPENSE.js
@@ -0,0 +1,26 @@
+export const DUMMY_EXPENSES = [
+    {
+        id: 1,
+        title: "Toilet Paper",
+        amount: 94.12,
+        date: new Date(2024, 7, 14),
+    },
+    {
+        id: 2,
+        title: "Rent",
+        amount: 1200,
+        date: new Date(2023, 12, 22),
+    },
+    {
+        id: 3,
+        title: "Groceries",
+        amount: 49.99,
+        date: new Date(2023, 5, 16),
+    },
+    {
+        id: 4,
+        title: "Skin Care",
+        amount: 99.12,
+        date: new Date(2024, 1, 1),
+    },
+];
